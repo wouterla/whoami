7bc1d95e9523559e1d5b0681ad309876869762b3
Wouter Lagerweij <wouter@lagerweij.com>
2016-04-29 15:30:47 +0200

    Print some environment variables

--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "strings"
   "os"
   "fmt"
   "net/http"
@@ -17,10 +18,16 @@ func main() {
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
-    })
+ 	      fmt.Fprintf(w, "I'm %s\n", hostname)
+        fmt.Println()
+        for _, e := range os.Environ() {
+            pair := strings.Split(e, "=")
+            if (strings.HasPrefix("pair[0]", "WHOAMI")) {
+              fmt.Println(w, "%s: %s", pair[0], pair[1])
+            }
+        }
 
+    })
 
     log.Fatal(http.ListenAndServe(":" + port, nil))
 }
-
